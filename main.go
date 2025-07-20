1cd13b5012aa561a441a470eb0d1d4ed71503886
Hyungsub (Danny) Kim <aa_aa_aa_11@naver.com>
2025-07-20 10:25:13 +0900

    feat: add cli command

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"god/proxy"
-	"log"
-)
+import "god/cmd/cli"
 
 func main() {
-	fmt.Println("Hello, G.O.D!")
-
-	// Proxy server start (temp)
-	// Forward Proxy command (window) : curl -x http://127.0.0.1:8083 http://httpbin.org/get
-	if err := proxy.Start("127.0.0.1:8083"); err != nil {
-		log.Fatalf("Proxy faild: %v", err)
-	}
+	cli.RunCLI()
 }
